28c6b7c4b268f71b9238e0d1b4a2b7f70a4a0737
gerardmatos <gerardmatos1990@gmail.com>
2023-09-29 09:47:53 -0500

    feat: change route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('../../var/spool/asterisk/monitor'))
 
 app.listen(3000, (req, res) => {
   console.log("server already")
